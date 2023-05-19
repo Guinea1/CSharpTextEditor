046e8f6360ca10901705fe4b82d23f4332aa6b64
Bobby D <bobbydapps@gmail.com>
2023-05-19 18:45:35 -0500

    Initial Commit

---
+++ CSharpTextEditor/MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.Win32;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+
+namespace CSharpTextEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        TextDocument doc = new TextDocument();
+        private void new_click(object sender, RoutedEventArgs e)
+        {
+            if (check_erase())
+            {
+                doc = new TextDocument();
+                text.Text = null;
+            }
+        }
+
+        private void open_click(object sender, RoutedEventArgs e)
+        {
+            if (check_erase())
+            {
+                bool result = (bool)open.ShowDialog();
+                if (result)
+                {
+                    doc.path = open.FileName;
+                    doc.open();
+                    text.Text = doc.contents;
+                }
+            }
+        }
+        public void save_click(object sender, RoutedEventArgs e)
+        {
+            if (doc.path == null)
+            {
+                saveas_click(sender, e);
+                return;
+            }
+            doc.contents = text.Text;
+            doc.save();
+        }
+
+        private void saveas_click(object sender, RoutedEventArgs e)
+        {
+            bool result = (bool)saveas.ShowDialog();
+            if (result)
+            {
+                doc.path = saveas.FileName;
+                doc.contents = text.Text;
+                doc.save();
+            }
+        }
+
+        private void exit_click(object sender, RoutedEventArgs e)
+        {
+            if (check_erase())
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void about_click(object sender, RoutedEventArgs e)
+        {
+            About about = new About();
+            about.ShowDialog();
+        }
+        private bool check_erase()
+        {
+            if (text.Text != doc.contents)
+            {
+                SaveChanges changes = new SaveChanges();
+                changes.ShowDialog();
+                bool result = changes.continueaction;
+                if (result)
+                {
+                    return true;
+                }
+                return false;
+            }
+            return true;
+        }
+
+        private void close_click(object sender, CancelEventArgs e)
+        {
+            if (!check_erase())
+            {
+                e.Cancel = true;
+            }
+        }
+
+        public void file_click(object sender, RoutedEventArgs e)
+        {
+            if (text.Text == doc.contents && !(text.Text == "" && doc.contents == ""))
+            {
+                save.IsEnabled = false;
+            }
+            else
+            {
+                save.IsEnabled = true;
+            }
+        }
+
+        public OpenFileDialog open = new OpenFileDialog
+        {
+            Filter = "Text files (*.txt)|*.txt"
+        };
+
+        public SaveFileDialog saveas = new SaveFileDialog
+        {
+            Filter = "Text files (*.txt)|*.txt"
+        };
+
+        class TextDocument
+        {
+            public string path { get; set; }
+            public string contents { get; set; } = "";
+
+            public void save()
+            {
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.WriteLine(contents);
+                }
+            }
+            public void open()
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (sr.EndOfStream == false)
+                    {
+                        contents += $"{sr.ReadLine()}\n";
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CSharpTextEditor/SaveChanges.xaml.cs
@@ -0,0 +1,60 @@
+﻿using CSharpTextEditor;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CSharpTextEditor
+{
+    /// <summary>
+    /// Interaction logic for SaveChanges.xaml
+    /// </summary>
+    public partial class SaveChanges : Window
+    {
+        public SaveChanges()
+        {
+            InitializeComponent();
+        }
+
+        public bool continueaction = true;
+        private bool appbutton = false;
+
+        private void save_click(object sender, RoutedEventArgs e)
+        {
+            (Application.Current.MainWindow as MainWindow).save_click(sender, e);
+            appbutton = true;
+            Close();
+        }
+
+        private void dontsave_click(object sender, RoutedEventArgs e)
+        {
+            appbutton = true;
+            Close();
+        }
+
+        private void cancel_click(object sender, RoutedEventArgs e)
+        {
+            appbutton = true;
+            continueaction = false;
+            Close();
+        }
+
+        private void close_click(object sender, CancelEventArgs e)
+        {
+            if (!appbutton)
+            {
+                continueaction = false;
+            }
+        }
+    }
+}
